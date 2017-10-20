b020e825c293876266da49b94e398d700ce95c65
Daniel Wangai <mainad57@gmail.com>
2017-10-20 20:28:34 +0300

    add authentication middleware.

---
+++ src/api/routes/authenticate.js
@@ -0,0 +1,27 @@
+import jwt from 'jsonwebtoken';
+
+const Authenticate = {
+  loginRequired: (req, res, next) => {
+    const token = req.headers['access-token'];
+    if(!token) {
+      return res.status(401).json({
+        status: 'fail',
+        message: 'You need to login to access this resource.'
+      });
+    }
+    jwt.verify(token, process.env.SECRET_KEY, (err, decoded) => {
+      if (error) {
+        return res.status(401).json({
+          status: 'fail',
+          message: 'Failed to authenticate.'
+        });
+      }
+      req.decoded = decoded;
+      req.user = req.user ? req.user : {};
+      req.user.roles = [decoded.data.role];
+      next();
+    })
+  }
+}
+
+export default Authenticate;
